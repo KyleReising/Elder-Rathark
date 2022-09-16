79dc84b60cbe1fee74587ebc4378fdb1e378cf3a
Kyle <reisingkyle07@gmail.com>
2022-09-15 22:26:05 -0400

    NPC can now view

--- Elder Rathark/Assets/Scripts/NpcScript.cs
+++ Elder Rathark/Assets/Scripts/NpcScript.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class NpcScript : MonoBehaviour
 {
+
+    [SerializeField] private detection detection;
+
+    // pathfinding & patrol
+    public Transform[] waypoints;
+    private int waypointIndex;
+    private float speed = 2f;
+    public float waitTime = 1f;
+    private float waitCounter = 0;
+    private bool waiting = false;
+    
+
+
+    public SpriteRenderer big_cheese;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +28,49 @@ public class NpcScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //patrol to dynamic waypoints
+
+        //chill for a second after reaching a waypoint
+        if (waiting)
+            {
+                waitCounter += Time.deltaTime;
+                if (waitCounter < waitTime)
+                    return;
+                waiting = false;
+            }
+            Transform wp = waypoints[waypointIndex];
+            //reached waypoint
+            if (Vector3.Distance(transform.position, wp.position) < 0.01f)
+            {
+                transform.position = wp.position;
+                waitCounter = 0f;
+                waiting = true;
+                waypointIndex = (waypointIndex + 1) % waypoints.Length;
+                
+            }
+            else  //move to waypoint
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, wp.position, speed * Time.deltaTime);
+                transform.position = temp;
+                
+                detection.SetOrigin(transform.position);
+            }
+            //look at waypoint
+            if (wp.position.x > transform.position.x)  //wp to right
+            {
+                detection.SetAimDirection(new Vector3(0.0f, 1.0f, 0f));
+                big_cheese.flipX = true;
+            }
+            else
+            {
+                detection.SetAimDirection(new Vector3(0.0f, -1.0f, 0f));
+                big_cheese.flipX = false;
+            }
+
+
         
+
     }
 
 
---
+++ Elder Rathark/Assets/Scripts/detection.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detection : MonoBehaviour
+{
+    [SerializeField] private LayerMask layermask;
+    private Mesh mesh;
+    public Transform pos;
+    private Vector3 origin = Vector3.zero;
+    private float startingAngle;
+    private float fov;
+
+    //helper function
+    Vector3 GetVectorFromAngle(float angle) 
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad),Mathf.Sin(angleRad));
+    }
+
+    //helper function
+    float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0)
+            n += 360;
+        return n;
+    }
+
+    private void Start()
+    {
+        mesh = new Mesh();
+        fov = 90f;
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    private void LateUpdate()
+    {
+        int rayCount = 50;
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 5f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D raycasthit2d = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layermask);
+            if (raycasthit2d.collider == null)
+            { vertex = origin + GetVectorFromAngle(angle) * viewDistance; }
+            else
+            { vertex = raycasthit2d.point; }
+        
+
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+            vertexIndex++;
+            angle -= angleIncrease;
+
+        }
+
+        mesh.vertices= vertices;
+        mesh.uv= uv;
+        mesh.triangles= triangles;
+     }
+
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        startingAngle = GetAngleFromVectorFloat(aimDirection) - fov / 2f;
+    }
+
+}
